d9fc8b6e1f8c8fa5659eaee1ba8251f8fffb1ad8
Thomas N. Jensen <thomasnorupjensen@gmail.com>
2025-12-07 14:32:59 +0100

    Upgraded next to version 15.4.8

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  /* Partial prerendering, still experimental */
+ /*  experimental:{
+    ppr: 'incremental'
+  } */
 };
 
 export default nextConfig;
